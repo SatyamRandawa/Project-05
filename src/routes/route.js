e01cb6444353c463200bbc1322a7229c63821bde
SatyamRandawa <satyamrandwa141@gmail.com>
2022-05-25 15:49:39 +0530

    4th api 90% done

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,7 @@ const UserController = require("../controller/userRegister")
 router.post("/register", UserController.createUser)
 router.post("/login", UserController.login)
 router.get("/get/user/:userId", UserController.getUser)
+router.put("/user/:userId/profile", UserController.updateProfile)
 
 
 
